9d4adff1c80f49712a40157b74f2222712c4bc24
kiannaquines <kjgnaquines@gmail.com>
2025-06-15 14:55:23 +0800

    added the migration files

---
+++ database/migrations/2025_06_15_064643_create_equipment_reservation_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('equipment_reservation', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('equipment_id')->references('id')->on('equipments');
+            $table->foreignId('study_space_id')->references('id')->on('study_space');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('equipment_reservation');
+    }
+};
