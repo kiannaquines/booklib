1bb44e607163b093550fedde8d8e6c7adbf8f500
kiannaquines <kjgnaquines@gmail.com>
2025-06-15 15:26:05 +0800

    added initial template per modules

---
+++ resources/js/pages/modules/book-reservation.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BookReservation = () => {
+  return (
+    <div>BookReservation</div>
+  )
+}
+
+export default BookReservation
---
+++ resources/js/pages/modules/equipment-reservation.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EquipmentReservation = () => {
+  return (
+    <div>EquipmentReservation</div>
+  )
+}
+
+export default EquipmentReservation
